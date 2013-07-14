755423e2eb94c8ecd4369b1bfc6b9abcb10418b4
Lina Po <lpovilaviciute@gmail.com>
2013-07-14 14:18:03 +0000

    Modified code to read index file

--- web.js
+++ web.js
@@ -1,13 +1,13 @@
-var express = require('express');
-
-var app = express.createServer(express.logger());
+//var express = require('express');
+var fs = require ('fs');
+//var app = express.createServer(express.logger());
 
 var outfile = "index.html";
 
-app.get('/', function(request, response) {
+//app.get('/', function(request, response) {
 //  response.send('Hello World 2!');
 fs.readFileSynch(outfile);
-});
+//});
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
