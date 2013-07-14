3a65e17eee4dd5ce0ab217e358c4dae46c2897b1
Lina Po <lpovilaviciute@gmail.com>
2013-07-14 14:11:11 +0000

    Added readFileSync

--- web.js
+++ web.js
@@ -2,8 +2,11 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var outfile = "index.html";
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+//  response.send('Hello World 2!');
+fs.readFileSynch(outfile);
 });
 
 var port = process.env.PORT || 5000;
